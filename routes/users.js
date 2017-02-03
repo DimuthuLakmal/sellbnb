807d91f1646f6bae242d74466703943183c521f3
dinukasal <dinukasal@gmail.com>
2017-02-03 16:19:20 +0530

    login checking with pwd encryption

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ var _ = require('lodash');
 var models = require('./../models');
 var sequelize = models.sequelize;
 var passport = require('passport');
-var passportLocal   = require('passport-local');
+var passportLocal = require('passport-local');
 var bcrypt = require('bcryptjs');
 var salt = bcrypt.genSaltSync(10);
 
@@ -13,52 +13,56 @@ passport.use(new passportLocal.Strategy(verifyCredentials));
 
 //it is not safe to use full user object to store in session. So only user id will be stored.
 passport.serializeUser(function (user, done) {
-  done(null, user.id);
+    done(null, user.id);
 });
 
 //Whenever needed we can use userid to extract other data of user.
-passport.deserializeUser(function(id, done) {
-  //retreive user object from db using userid
-  var User = models.User;
-  User.findAll({
-    where: {
-        id: id,
-    }
-  }).then(function (User) {
-    var user = User[0].dataValues;
-    done(null, user);
-  });
+passport.deserializeUser(function (id, done) {
+    //retreive user object from db using userid
+    var User = models.User;
+    User.findAll({
+        where: {
+            id: id,
+        }
+    }).then(function (User) {
+        var user = User[0].dataValues;
+        done(null, user);
+    });
 
 });
 
 //verffying username and password
 function verifyCredentials(username, password, done) {
 
-  sequelize.sync().then(
-      function () {
-        var User = models.User;
+    sequelize.sync().then(
+        function () {
+            var User = models.User;
 
-        User.findAll({
-          where: {
-            username: username,
-            password: bcrypt.hashSync(password, salt),
-          }
-        }).then(function (User) {
-          if(!_.isUndefined(User[0])) {
-            var user = User[0].dataValues;
-            done(null, {id: user.id});
-          } else {
-            //set error message to flash
-            done(null, false, { message: 'Invalid Username or Password!' } );
-          }
-        });
-      }
-  );
+            User.findAll({
+                where: {
+                    username: username,
+                    //password: bcrypt.compareSync(password, hash),
+                }
+            }).then(function (User) {
+                console.log(User[0].dataValues.password);
+                if (!_.isUndefined(User[0])) {
+                    if (bcrypt.compareSync(password, User[0].dataValues.password)) {    //checking password
+                        var user = User[0].dataValues;
+                        done(null, {id: user.id});
+                        console.log("login success!");
+                    }
+                } else {
+                    //set error message to flash
+                    done(null, false, {message: 'Invalid Username or Password!'});
+                }
+            });
+        }
+    );
 }
 
 /* GET users listing. */
-router.get('/login', function(req, res, next) {
-  res.render('login', {message: req.flash("error")});
+router.get('/login', function (req, res, next) {
+    res.render('login', {message: req.flash("error")});
 });
 
 /* POST request from login form. User passport local method for authentication */
@@ -91,12 +95,12 @@ router.post('/password', function (req, res) {
                     password: currentpassword,
                 }
             }).then(function (User) {
-                if(!_.isUndefined(User[0])) {
+                if (!_.isUndefined(User[0])) {
                     var user = User[0].dataValues;
                     var User = models.User;
                     User.update(
-                        { password: newpassword },
-                        { where: { id: userId } }
+                        {password: newpassword},
+                        {where: {id: userId}}
                     ).then(function (results) {
                         res.redirect('/user/basic');
                     });
@@ -113,12 +117,25 @@ router.post('/password', function (req, res) {
 
 router.post('/adduser', function (req, res) {
     if (typeof(req.body.username) != "undefined" && typeof req.body.password != "undefined") {
-        var hash = bcrypt.hashSync(req.body.password[0], salt);
-        models.User.create({username: req.body.username, password: hash}).then(function () {
 
-            console.log(hash);
-            res.redirect('/user/basic');
-        })
+        models.User.findAll({
+            where: {
+                username: req.body.username,
+            }
+        }).then(function (User) {
+            if (!_.isUndefined(User[0])) {
+                res.redirect('/signup', {message: 'Username already exists!'});
+            } else {
+                //set error message to flash
+                //done(null, false, { message: 'Wrong username or password!' } );
+                var hash = bcrypt.hashSync(req.body.password[0], salt);
+                models.User.create({username: req.body.username, password: hash}).then(function () {
+                    console.log(hash);
+                    res.redirect('/user/basic');
+                });
+            }
+        });
+
     } else {
         res.send("error in adding user!");
         res.status(err.status || 500);
@@ -128,6 +145,14 @@ router.post('/adduser', function (req, res) {
         });
     }
 
+});
+
+router.get('/delall', function () {
+    models.User.destroy({
+        where: {}
+    }).then(function () {
+        res.send("ok");
+    });
 });
 
 module.exports = router;
