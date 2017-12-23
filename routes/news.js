ef60272a8bd406ba31c02cee638194f8b519a996
malaka <malaka.14@cse.mrt.ac.lk>
2017-12-23 14:56:11 +0530

    two raw news line

--- routes/news.js
+++ routes/news.js
@@ -319,7 +319,7 @@ router.get('/viewlatest', function (req, res) {
             var News = models.News;
             var User = models.User;
             News.findAndCountAll({
-                limit: 4,
+                limit: 8,
                 include: [User],
                 order: '`createdAt` DESC'
             }).then(function (News) {
